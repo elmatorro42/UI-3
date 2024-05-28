1605c1f09f1ce6bedf964b67885c069f3ba6401c
elmatorro42 <matiasschvartzberg@gmail.com>
2024-05-28 09:06:39 -0300

    Password

---
+++ Assets/LOGINMANAGER.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LOGINMANAGER : MonoBehaviour
+{
+    public string Contras = "prit";
+    public InputField IngresarContraseña;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Contra()
+    {
+        if(IngresarContraseña.text == Contras)
+        {
+            Debug.Log("Access granted");
+        }
+        else
+        {
+            Debug.Log("Access Denied");
+        }
+    }
+}
